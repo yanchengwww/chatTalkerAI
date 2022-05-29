26df4d129f18b7c9277275136bc930316d2b6a58
yanchengwww <wt951753@gmail.com>
2022-05-29 19:32:29 +0800

    delete

--- js/all.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.gotop').on('click', function (event) {
-        event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 1500);
-    });
-});
