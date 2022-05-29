d8c965e2aad2d8a90c7aa5b5e73182def8c6a15e
yanchengwww <wt951753@gmail.com>
2022-05-29 23:13:12 +0800

    chatTalker_new

--- js/all.js
+++ js/all.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
         event.preventDefault();
         $('html, body').animate({scrollTop: 0}, 1500);
     });
+
+    $('.QA-title').click(function (e) {
+        e.preventDefault();
+
+        $(this).toggleClass('blue');  //自己變藍
+        $(this).parent().parent().siblings().find('.QA-title').removeClass('blue');  //別人變黑
+
+        $(this).toggleClass('QA-plus').toggleClass('QA-minus');  //自己變+
+        $(this).parent().parent().siblings().find('.QA-title').removeClass('QA-minus').addClass('QA-plus');  //別人變-
+
+        $(this).parent().siblings('.QA-answer').slideToggle();  //自己展開
+        $(this).parent().parent().siblings().find('.QA-answer').slideUp();  //別人收闔
+
+    });
+
 });
